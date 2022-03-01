54a48d995e8455435ae14665d426e0dea2f6edc9
Leo Miao <miaoleo8554@gmail.com>
2022-03-01 17:20:12 -0600

    Add files via upload

---
+++ filter.sh
@@ -0,0 +1,7 @@
+# @Author: leomiao
+# @Date:   2022-03-01 16:04:14
+# @Last Modified by:   leomiao
+# @Last Modified time: 2022-03-01 16:05:09
+#!/bin/bash
+python unzip.py
+python match_uniprot_id.py
