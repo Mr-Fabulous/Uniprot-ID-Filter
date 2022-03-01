56f298f55864cd7e37d9fb9981428788c19955a4
Leo Miao <miaoleo8554@gmail.com>
2022-03-01 16:37:27 -0600

    Add files via upload

---
+++ unzip.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# @Author: leomiao
+# @Date:   2022-03-01 14:00:30
+# @Last Modified by:   leomiao
+# @Last Modified time: 2022-03-01 15:33:44
+# import required module
+import os
+import tarfile
+from pathlib import Path
+
+# assign directory
+directory = './Alphafold'
+ 
+# iterate over files in the directory
+for filename in os.listdir(directory):
+    f = os.path.join(directory, filename)
+    # checking if it is a file
+    if os.path.isfile(f):
+        if filename.endswith((".tar")):
+            folder_path = os.path.join(directory, filename[:-4])
+            if not os.path.exists(folder_path):
+                os.makedirs(folder_path)
+            tar = tarfile.open(f)
+            print("Extracting ", folder_path, "...")
+            tar.extractall(path=folder_path)
+            tar.close()
